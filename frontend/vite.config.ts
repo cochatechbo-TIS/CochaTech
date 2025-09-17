b2c67bbe3b29fc578e4b4d219022d4e60bd8de6b
AxelOBT <axelob1010@gmail.com>
2025-09-17 11:02:51 -0400

    Cambios fusionados

---
+++ frontend/vite.config.ts
@@ -0,0 +1,33 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    hmr: {
+      port: 3000
+    },
+    watch: {
+      usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@store': path.resolve(__dirname, './src/store')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
