86e2b869892d231717b253f0351d08e3ed7282de
romaJM <romane.jacha@gmail.com>
2025-10-04 04:16:47 -0400

    llenando datos a la tabla area

--- backend/database/migrations/2025_10_04_081319_insert_area.php
+++ backend/database/migrations/2025_10_04_081319_insert_area.php
@@ -1,24 +1,25 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        DB::table('area')->insert([
+            ['id_area' => 1, 'nombre_area' => 'Matemática'],
+            ['id_area' => 2, 'nombre_area' => 'Física'],
+            ['id_area' => 3, 'nombre_area' => 'Química'],
+            ['id_area' => 4, 'nombre_area' => 'Biología'],
+            ['id_area' => 5, 'nombre_area' => 'Astronomía'],
+            ['id_area' => 6, 'nombre_area' => 'Geografía'],
+            ['id_area' => 7, 'nombre_area' => 'Informática'],
+        ]);
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        DB::table('area')->whereIn('id_area', [1,2,3,4,5,6,7])->delete();
     }
 };
