6e2c5ed4d3379860d824b58772937717217e6907
romaJM <romane.jacha@gmail.com>
2025-10-17 17:12:14 -0400

    middleware por rol

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Gestion_Olimpista_Controller;
@@ -13,26 +15,26 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Rutas de gestión general
-    Route::get('/olimpistas', [Gestion_Olimpista_Controller::class, 'index']);
-    Route::put('/olimpistas/{id}', [Gestion_Olimpista_Controller::class, 'update']);
-    Route::delete('/olimpistas/{id}', [Gestion_Olimpista_Controller::class, 'destroy']);
-    Route::post('/olimpistas/importar', [Importar_Olimpista_Controller::class, 'importar']);
+    // Grupo solo para ADMIN
+    Route::middleware('role:administrador')->group(function () {
+        Route::get('/olimpistas', [Gestion_Olimpista_Controller::class, 'index']);
+        Route::put('/olimpistas/{id}', [Gestion_Olimpista_Controller::class, 'update']);
+        Route::delete('/olimpistas/{id}', [Gestion_Olimpista_Controller::class, 'destroy']);
+        Route::post('/olimpistas/importar', [Importar_Olimpista_Controller::class, 'importar']);
 
-    Route::get('/responsable', [Responsable_Area_Controller::class, 'index']);
-    Route::post('/responsable', [Responsable_Area_Controller::class, 'store']);
-    Route::put('/responsable/{id}', [Responsable_Area_Controller::class, 'update']);
-    Route::delete('/responsable/{id}', [Responsable_Area_Controller::class, 'destroy']);
+        Route::get('/responsable', [Responsable_Area_Controller::class, 'index']);
+        Route::post('/responsable', [Responsable_Area_Controller::class, 'store']);
+        Route::put('/responsable/{id}', [Responsable_Area_Controller::class, 'update']);
+        Route::delete('/responsable/{id}', [Responsable_Area_Controller::class, 'destroy']);
+    });
 
-    Route::get('/evaluador', [Evaluador_Controller::class, 'index']);
-    Route::post('/evaluador', [Evaluador_Controller::class, 'store']);
-    Route::put('/evaluador/{id}', [Evaluador_Controller::class, 'update']);
-    Route::delete('/evaluador/{id}', [Evaluador_Controller::class, 'destroy']);
+    // Grupo para ADMIN y RESPONSABLE
+    Route::middleware('role:administrador,responsable')->group(function () {
+        Route::get('/evaluador', [Evaluador_Controller::class, 'index']);
+        Route::post('/evaluador', [Evaluador_Controller::class, 'store']);
+        Route::put('/evaluador/{id}', [Evaluador_Controller::class, 'update']);
+        Route::delete('/evaluador/{id}', [Evaluador_Controller::class, 'destroy']);
 
-    Route::post('/area-nivel', [Area_Nivel_Controller::class, 'generarYListar']);
-
-    // Rutas exclusivas para admin
-    Route::middleware('is_admin')->group(function () {
-        Route::post('/admin/users', [UserController::class, 'store']);
+        Route::post('/area-nivel', [Area_Nivel_Controller::class, 'generarYListar']);
     });
 });
